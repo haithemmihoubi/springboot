d2641c88379238c41f926c92bfd0c56859eaa784
haith <haithemmihoubi1234@gmail.com>
2022-10-16 05:31:08 +0100

    Initial commit

---
+++ src/test/java/com/haithem/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haithem.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
